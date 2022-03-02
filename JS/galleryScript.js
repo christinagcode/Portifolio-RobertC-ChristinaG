e3bf85f7f19b95b2109d04e50a70d22bce433202
christinagcode <christinagooarts@gmail.com>
2022-03-02 16:57:11 -0600

    testing

--- JS/galleryScript.js
+++ JS/galleryScript.js
@@ -21,3 +21,62 @@ window.addEventListener('scroll', function(){
 
 }
 )
+
+// twinkle stars canvas 
+const twilights = [];
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+function init() {
+  const banner = document.getElementById("banner");
+  canvas.width = banner.offsetWidth;
+  canvas.height = banner.offsetHeight;
+  for (let i = 0; i < 200; i++) {
+    twilights.push({
+      x: Math.random(),
+      y: Math.random(),
+      size: Math.random(),
+      change: 0.15,
+    });
+  }
+}
+function update() {
+  for (const light of twilights) {
+    light.x += 0.01;
+    if (light.x > 1) {
+        light.x = 0;
+    }
+    if (light.size < 0.1) {
+        light.change = 0.1;
+    } else if (light.size > 0.9) {
+        light.change = -0.1;
+    }
+    light.size += light.change;
+  }
+}
+function render() {
+  const { width, height } = canvas;
+  context.clearRect(0, 0, width, height); 
+  for (const light of twilights) {
+    context.beginPath();
+    context.arc(
+        light.x * width,
+        light.y * height,
+        light.size * 3,
+      0,
+      2 * Math.PI,
+      false
+    );
+    context.fillStyle = "white";
+    context.fill();
+  }
+}
+
+function twinkle() {
+  update();
+  render();
+}
+setInterval(twinkle, 100);
+init();
+
+render();
